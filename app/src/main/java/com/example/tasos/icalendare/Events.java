de42bc74c77fc38dab27272242044544b32893d5
Unknown <anastasiosfortis@gmail.com>
2018-05-22 18:35:51 +0300

    Remove unused imports

---
+++ app/src/main/java/com/example/tasos/icalendare/Events.java
@@ -0,0 +1,10 @@
+package com.example.tasos.icalendare;
+
+import java.sql.Time;
+
+public class Events {
+    String title;
+    Time startTime;
+    Time endTime;
+
+}
---
+++ app/src/main/java/com/example/tasos/icalendare/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.tasos.icalendare;
+
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //Arxikopoihsi ths ActioBar bilio8ikh
+        ActionBar mActionBar = getSupportActionBar();
+        ActionBarInit actionBarInit = new ActionBarInit(mActionBar, getBaseContext());
+        actionBarInit.initActionBar();
+
+
+
+    }
+}
